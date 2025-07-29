d9ab5d79797393be38f96dd285d6d9cb74bc79d4
pradeepmeena05 <pradeepmeena34330@gmail.com>
2025-07-29 11:04:51 +0530

    Deploy updated web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3090812811"
+    serviceWorkerVersion: "1568976745"
   }
 });
